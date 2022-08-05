0f8e3bbc925b3e474178260aaeb888696f68e9aa
Rian <rian.ace.2003@gmail.com>
2022-08-04 22:38:00 -0300

    finalizado

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,6 +3,9 @@ const D = document.getElementById("direita");
 const cont = document.getElementById("container");
 var count = 0;
 
+E.addEventListener("click", vaiEsquerda);
+D.addEventListener("click", vaiDireita);
+
 function vaiEsquerda(){
     if(count === 0){
         return;
@@ -18,6 +21,7 @@ function vaiEsquerda(){
 function vaiDireita(){
     if(count === 0){
         cont.style.right = "400px";
+        cont.style.transition = "all 0.5s";
         return count++;
     }else if(count === 1){
         cont.style.right = "800px";
@@ -27,3 +31,20 @@ function vaiDireita(){
         return count = 0;
     }
 }
+
+// ----show-imgs----
+
+const botao = document.getElementById("dark");
+const body = document.getElementsByTagName("body")[0];
+const opcao = document.getElementById("dark-light");
+const darkMode = 'dark-mode';
+
+function changes(){
+    body.classList.toggle(darkMode);
+    E.classList.toggle(darkMode);
+    D.classList.toggle(darkMode);
+    opcao.classList.toggle(darkMode);
+    botao.classList.toggle(darkMode);
+}
+
+botao.addEventListener("click", changes);
